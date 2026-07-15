05274293f3f86263ae823a543dd7d27f9bc22d3a
wenhaozhao <wenhaozhao@sohu-inc.com>
2026-07-15 14:39:22 +0800

    Use precompiled binaries for rload

--- Formula/rload.rb
+++ Formula/rload.rb
@@ -1,14 +1,22 @@
 class Rload < Formula
   desc "Rust HTTP load generator with Nginx access-log and JSONL replay"
   homepage "https://wenhaozhao.github.io/rload/"
-  url "https://github.com/wenhaozhao/rload/archive/refs/tags/v0.2.3.tar.gz"
-  sha256 "606f02b8bf18b56c5cf3aca7a804da15f75e5bd3a6f5136f1e64ae8b296f0811"
   license any_of: ["MIT", "Apache-2.0"]
 
-  depends_on "rust" => :build
+  on_arm do
+    on_macos do
+      url "https://github.com/wenhaozhao/rload/releases/download/v0.2.3/rload-0.2.3-aarch64-apple-darwin.tar.gz"
+      sha256 "9731aac405f8a20d1f362028fbea11857845b786d53a14879a5abb37e1aa0dfb"
+    end
+  end
+
+  on_linux do
+    url "https://github.com/wenhaozhao/rload/releases/download/v0.2.3/rload-0.2.3-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "0eeff63df792c14da712b11bb41e7e93f75577258c9ebcc17de4da37d1f7f5fe"
+  end
 
   def install
-    system "cargo", "install", *std_cargo_args(path: ".")
+    bin.install "rload"
   end
 
   test do
